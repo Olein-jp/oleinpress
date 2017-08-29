59c232c82f815091a486fcee51888939de24b6b0
KojiKuno <koji.kuno@gmail.com>
2017-08-29 12:36:39 +0900

    made footer

--- footer.php
+++ footer.php
@@ -14,17 +14,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'oleinpress' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'oleinpress' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'oleinpress' ), 'oleinpress', '<a href="http://olein-design.com">Koji Kuno</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="site-footer__inner container">
+			<div class="copyright-bar">
+				<p>Copyright © <?php bloginfo( 'blogname' ); ?> by
+				<?php
+					printf( esc_html__( '%1$s.', 'oleinpress' ), '<a href="http://demo.olein-design.com/oleinpress/">OleinPress</a>' );
+				?>
+				</p>
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
