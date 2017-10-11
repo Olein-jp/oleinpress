2adf2db8eeae6c126a05f62d4885e3360474c771
KojiKuno <koji.kuno@gmail.com>
2017-10-12 05:56:58 +0900

    1.0.7
    Removed include breadcrumb.php on functions.php

--- functions.php
+++ functions.php
@@ -163,8 +163,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-/**
- * Breadcrumbs fot this theme
- */
-require get_template_directory() . '/inc/breadcrumb.php';
